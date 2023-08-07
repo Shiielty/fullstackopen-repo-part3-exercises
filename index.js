357f1501d81d6276ea2f12cde5efdae2421cdc1d
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-08-07 12:09:30 +0700

    Finished exercise 3.3

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    console.log("Status: 404");
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`server listening to port ${PORT}`);
