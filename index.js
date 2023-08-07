bf791a52b378f692643593d2700636a2f538a071
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-08-07 19:50:48 +0700

    Finished exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
 app.use(
   morgan(function (tokens, req, res) {
     if (tokens.method(req, res) === "POST") {
@@ -124,7 +127,7 @@ app.post("/api/persons", (request, response) => {
   response.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server listening to port ${PORT}`);
 });
